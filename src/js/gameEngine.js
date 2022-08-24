b7c29cd8475f70a73d7439e25d820ff8193504e5
Pavel.Dimitrov <2heads.pd@gmail.com>
2022-08-24 16:59:58 +0300

    Wizard movement with boundaries

--- src/js/gameEngine.js
+++ src/js/gameEngine.js
@@ -4,29 +4,34 @@ function start(state, game) {
 }
 
 function gameLoop(state, game) {
-    const { wizard } = state
-    const { wizardElement } = game
-    //Move wizard
+    const { wizard } = state;
+    const { wizardElement } = game;
 
-    if (state.keys.KeyA) {
-        wizard.posX -= wizard.speed
-    };
+    modifyWizardPosition(state,game)
 
-    if (state.keys.KeyS) {
-        wizard.posY += wizard.speed
-    };
-
-    if (state.keys.KeyD) {
-        wizard.posX += wizard.speed
-    };
-
-    if (state.keys.KeyW) {
-        wizard.posY -= wizard.speed
-    };
 
 
     //Render
     wizardElement.style.left = wizard.posX + 'px';
     wizardElement.style.top = wizard.posY + 'px';
     window.requestAnimationFrame(gameLoop.bind(null, state, game))
+}
+
+function modifyWizardPosition(state,game){
+    const {wizard} = state
+    if (state.keys.KeyA) {
+        wizard.posX = Math.max(wizard.posX - wizard.speed, 0)
+    };
+
+    if (state.keys.KeyS) {
+        wizard.posY = Math.min(wizard.posY + wizard.speed, game.gameScreen.offsetHeight-wizard.height)
+    };
+
+    if (state.keys.KeyD) {
+        wizard.posX = Math.min(wizard.posX + wizard.speed, game.gameScreen.offsetWidth-wizard.width)
+    };
+
+    if (state.keys.KeyW) {
+        wizard.posY = Math.max(wizard.posY - wizard.speed, 0)
+    };
 }
